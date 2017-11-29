152a9f31f053d652bdf562addc0450dd1a014c3f
Bryce Murray <brycepmurray711@gmail.com>
2017-11-29 14:13:41 -0700

    Boards

--- www/src/store/index.js
+++ www/src/store/index.js
@@ -3,76 +3,96 @@ import vue from 'vue'
 import vuex from 'vuex'
 
 let api = axios.create({
-  baseURL: 'http://localhost:3000/api/',
-  timeout: 2000,
-  withCredentials: true
+    baseURL: 'http://localhost:3000/api/',
+    timeout: 2000,
+    withCredentials: true
 })
 
 let auth = axios.create({
-  baseURL: 'http://localhost:3000/',
-  timeout: 2000,
-  withCredentials: true
+    baseURL: 'http://localhost:3000/',
+    timeout: 2000,
+    withCredentials: true
 })
 vue.use(vuex)
 
 var store = new vuex.Store({
-  state: {
-    boards: [{name: 'This is total rubbish'}],
-    activeBoard: {},
-    error: {}
-  },
-  mutations: {
-    setBoards(state, data){
-      state.boards = data
+    state: {
+        boards: [{ name: 'This is total rubbish' }],
+        activeBoard: {},
+        error: {},
+        user: {}
     },
-    handleError(state, err){
-      state.error = err
-    }
-  },
-  actions: {
-    //when writing your auth routes (login, logout, register) be sure to use auth instead of api for the posts
+    mutations: {
+        setBoards(state, data) {
+            state.boards = data
+        },
+        handleError(state, err) {
+            state.error = err
+        },
+        setUser(state, user) {
+            state.user = user
+        }
+    },
+    actions: {
+        //when writing your auth routes (login, logout, register) be sure to use auth instead of api for the posts
 
-    getBoards({commit, dispatch}) {
-      api('boards')
-        .then(res => {
-          commit('setBoards', res.data.data)
-        })
-        .catch(err=>{
-          commit('handleError', err)
-        })
-    },
-    getBoard({commit, dispatch},id) {
-      api('boards/' + id)
-        .then(res => {
-          commit('setActiveBoard', res.data.data)
-        })
-        .catch(err=>{
-          commit('handleError', err)
-        })
-    },
-    createBoard({commit, dispatch}, board) {
-      debugger
-      api.post('boards/',board)
-        .then(res => {
-          dispatch('getBoards')
-        })
-        .catch(err=>{
-          commit('handleError', err)
-        })
-    },
-    removeBoard({commit, dispatch}, board) {
-      api.delete('boards/'+board._id)
-        .then(res => {
-          this.getBoards()
-        })
-        .catch(err=>{
-          commit('handleError', err)
-        })
-    },
-    handleError({commit, dispatch}, err){
-      commit('handleError', err)
+        //THESE ARE ALL BOARD FUNCTIONS
+        getBoards({ commit, dispatch }) {
+            api('boards')
+                .then(res => {
+                    commit('setBoards', res.data.data)
+                })
+                .catch(err => {
+                    commit('handleError', err)
+                })
+        },
+        getBoard({ commit, dispatch }, id) {
+            api('boards/' + id)
+                .then(res => {
+                    commit('setActiveBoard', res.data.data)
+                })
+                .catch(err => {
+                    commit('handleError', err)
+                })
+        },
+        createBoard({ commit, dispatch }, board) {
+            debugger
+            api.post('boards/', board)
+                .then(res => {
+                    dispatch('getBoards')
+                })
+                .catch(err => {
+                    commit('handleError', err)
+                })
+        },
+        removeBoard({ commit, dispatch }, board) {
+            api.delete('boards/' + board._id)
+                .then(res => {
+                    this.getBoards()
+                })
+                .catch(err => {
+                    commit('handleError', err)
+                })
+        },
+        //ERROR FUNCTIONS
+        handleError({ commit, dispatch }, err) {
+            commit('handleError', err)
+        },
+        //LOGIN FUNCTIONS
+        login({ commit, dispatch }, payload) {
+            debugger
+            auth.post('login', payload)
+                .then(res => {
+                    commit('setUser', res.data.data)
+                    router.push({ name: 'Boards' })
+                    console.log(res)
+                })
+                .catch(err => {
+                    commit('handleError', err)
+                })
+        }
+
     }
-  }
 
 })
 
