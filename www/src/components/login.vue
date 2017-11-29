1585804807becf96fa276c0cc4aeea3006bad32a
Bryce Murray <brycepmurray711@gmail.com>
2017-11-29 13:35:20 -0700

    login/registration

---
+++ www/src/components/login.vue
@@ -0,0 +1,93 @@
+<template>
+        <div class="login">
+                <h1>Welcome To Login Page</h1>
+                <p>Please login to continue:</p>
+                <!-- Add error message if login fails -->
+                <h5 class="text-danger" v-if="error"><b>{{error}}</b></h5>
+                <div v-if="loginForm" class="login">
+                    <form class="form" @submit.prevent="submitLogin">
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input class="form-control" type="email" name="email" placeholder="bob@bob.co" v-model='login.email' required>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input class="form-control" type="password" name="password" v-model='login.password' required>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit">Login</button>
+                        </div>
+                    </form>
+                </div>
+                <div v-else class="register">
+                    <form class="form" @submit.prevent="submitRegister">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input class="form-control" type="text" name="name" placeholder="name" v-model='register.name' required>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input class="form-control" type="email" name="email" placeholder="bob@bob.co" v-model='register.email' required>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input class="form-control" type="password" name="password" v-model='register.password' required>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit">Register</button>
+                        </div>
+                    </form>
+                </div>
+                <p v-if="loginForm" @click="toggleLoginForm" class="action">Not a user? Click here to register</p>
+                <p v-else @click="toggleLoginForm" class="action">Already a user? Click here to login</p>
+            </div>
+        </template>
+<script>
+    export default {
+        data() {
+            return {
+                loginForm: true,
+                login: {
+                    email: '',
+                    password: ''
+                },
+                register: {
+                    name: '',
+                    email: '',
+                    password: ''
+                }
+            }
+        },
+        methods: {
+            toggleLoginForm() {
+                this.loginForm = !this.loginForm
+            },
+            submitLogin() {
+                this.$store.dispatch('login', this.login)
+                this.login = {
+                    email: '',
+                    password: ''
+                }
+            },
+            submitRegister() {
+                this.$store.dispatch('register', this.register)
+            }
+        },
+        computed: {
+            error() {
+                return this.$store.state.error.message
+            }
+        }
+    }
+</script>
+<style>
+    .action {
+        cursor: pointer;
+        opacity: .7;
+        transition: all .3s linear;
+    }
+    
+    .action:hover {
+        opacity: 1
+    }
+</style>
--- www/src/router/index.js
+++ www/src/router/index.js
@@ -2,19 +2,22 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Boards from 'components/Boards'
 import Board from 'components/Board'
-
+import Login from 'components/Login'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Boards',
-      component: Boards
-    },{
-      path: '/boards/:id',
-      name: 'Board',
-      component: Board
-    }
-  ]
+    routes: [{
+        path: '/',
+        name: 'Boards',
+        component: Boards
+    }, {
+        path: '/boards/:id',
+        name: 'Board',
+        component: Board
+    }, {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    }],
+
 })
