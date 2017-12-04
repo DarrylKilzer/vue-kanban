d3054c1cc7607b7aecda47fe9676fa4e618e5f36
Chase Shank <216cds@gmail.com>
2017-12-03 19:25:11 -0700

    styling

--- www/src/App.vue
+++ www/src/App.vue
@@ -1,9 +1,14 @@
 <template>
+  <body>
+    <div class="container-fluid main">
+    
   <div id="app">
-    <error></error>
-    <button @click="logout">Logout</button>
+    <a><h3 @click="logout" class="logout">Log Out</h3></a>
     <router-view></router-view>
   </div>
+</div>
+</body>
+
 </template>
 
 <script>
@@ -33,4 +38,26 @@
     color: #2c3e50;
     margin-top: 60px;
   }
+
+  .logout {
+    list-style-type: none;
+    margin-right: 10px;
+    padding: 3px;
+    overflow: hidden;
+    float: right;
+    cursor: pointer;
+    color: rgb(134, 18, 18);
+    outline: 1px solid black
+}
+
+.logout:hover{
+  background-color: rgba(172, 0, 0, 0.87);
+  color: rgb(255, 255, 255);
+  transition: .3s
+}
+.main{
+  height: 100vh;
+  background-color: rgba(104, 42, 19, 0.185)
+}
+
 </style>
--- www/src/components/login.vue
+++ www/src/components/login.vue
@@ -1,47 +1,53 @@
 <template>
-        <div class="login">
-                <h1>Welcome To Login Page</h1>
-                <p>Please login to continue:</p>
-                <!-- Add error message if login fails -->
-                <h5 class="text-danger" v-if="error"><b>{{error}}</b></h5>
-                <div v-if="loginForm" class="login">
-                    <form class="form" @submit.prevent="submitLogin">
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input class="form-control" type="email" name="email" placeholder="bob@bob.co" v-model='login.email' required>
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input class="form-control" type="password" name="password" v-model='login.password' required>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit">Login</button>
-                        </div>
-                    </form>
+    <div class="login">
+        <div class="row text-center">
+            <div class="col-xs-12">
+        <h1 v-if="loginForm">Log in to view boards.</h1>
+        <h1 v-else>Please register to continue.</h1>
+    </div>
+    </div>
+        <!-- Add error message if login fails -->
+        <h5 class="text-danger" v-if="error">
+            <b>{{error}}</b>
+        </h5>
+        <div v-if="loginForm" class="login">
+            <form class="form" @submit.prevent="submitLogin">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input class="form-control" type="email" name="email" placeholder="bob@bob.co" v-model='login.email' required>
                 </div>
-                <div v-else class="register">
-                    <form class="form" @submit.prevent="submitRegister">
-                        <div class="form-group">
-                            <label for="name">Name</label>
-                            <input class="form-control" type="text" name="name" placeholder="name" v-model='register.name' required>
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input class="form-control" type="email" name="email" placeholder="bob@bob.co" v-model='register.email' required>
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input class="form-control" type="password" name="password" v-model='register.password' required>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit">Register</button>
-                        </div>
-                    </form>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input class="form-control" type="password" placeholder="********" name="password" v-model='login.password' required>
                 </div>
-                <p v-if="loginForm" @click="toggleLoginForm" class="action">Not a user? Click here to register</p>
-                <p v-else @click="toggleLoginForm" class="action">Already a user? Click here to login</p>
-            </div>
-        </template>
+                <div class="form-group">
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+        </div>
+        <div v-else class="register">
+            <form class="form" @submit.prevent="submitRegister">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input class="form-control" type="text" name="name" placeholder="Bob" v-model='register.name' required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input class="form-control" type="email" name="email" placeholder="bob@email.com" v-model='register.email' required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input class="form-control" type="password" name="password" placeholder="********" v-model='register.password' required>
+                </div>
+                <div class="form-group">
+                    <button type="submit">Register</button>
+                </div>
+            </form>
+        </div>
+        <p v-if="loginForm" @click="toggleLoginForm" class="action">Not a user? Click here to register</p>
+        <p v-else @click="toggleLoginForm" class="action">Already a user? Click here to login</p>
+    </div>
+</template>
 <script>
     export default {
         data() {
@@ -83,11 +89,32 @@
 <style>
     .action {
         cursor: pointer;
-        opacity: .7;
-        transition: all .3s linear;
+        opacity: .8;
+        transition: all .2s linear;
+        color: rgb(8, 76, 121);
+        font-size: 1.2em
     }
-    
+
     .action:hover {
         opacity: 1
     }
+
+    
+    .form {
+        width: 100vw;
+        padding-left: 20vw;
+        padding-right: 20vw;
+        text-align: center;
+        
+    }
+
+    .form-control{
+        text-align: center;
+        
+    }
+
+    .login{
+        background-color: rgba(2, 74, 102, 0.11)
+    }
+
 </style>
